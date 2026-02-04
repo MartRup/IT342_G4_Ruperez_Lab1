1a974039dc292067425d8134932da28af6b48d5f
LAB13_USER ON WS40 <L13X16W40@LabHCI.net>
2026-02-04 16:27:16 +0800

    Your message

---
+++ backend/miniapplication/src/test/java/com/example/miniapplication/MiniapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
